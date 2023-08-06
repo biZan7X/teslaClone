d732a4f3ce24127811fc42a85132c93bfed6c484
BT <biswanath@geekyants.com>
2023-08-06 13:32:09 +0530

    added some random style

--- components/CarItem/styles.js
+++ components/CarItem/styles.js
@@ -1,36 +1,37 @@
-import { Dimensions, StyleSheet } from "react-native";
+import {Dimensions, StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
-    carContainer: {
-        width: '100%',
-        height: Dimensions.get('window').height,
-        alignItems: 'center',
-      },
-      titles: {
-        marginTop: '30%',
-        width: '100%',
-        alignItems: 'center',
-      },
-      title: {
-        fontSize: 40,
-        fontWeight: '500',
-      },
-      subtitle: {
-        fontSize: 16,
-        color: "#5c5e62"
-      },
-      subtitleCTA: {
-        textDecorationLine: 'underline',
-      },
-      image: {
-        width: '100%',
-        height: '100%',
-        resizeMode: 'cover',
-        position: 'absolute',
-      },
-      bottonContainer: {
-          position: "absolute",
-          bottom: 50,
-          width: "100%"
-      }
+  carContainer: {
+    width: '100%',
+    height: Dimensions.get('window').height,
+    alignItems: 'center',
+  },
+  titles: {
+    marginTop: '30%',
+    width: '100%',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: '500',
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#5c5e62',
+  },
+  subtitleCTA: {
+    textDecorationLine: 'underline',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+    position: 'absolute',
+  },
+  bottonContainer: {
+    position: 'absolute',
+    bottom: 50,
+    width: '100%',
+  },
+  someRandomStyle: {},
 });
